b4176791d50173d2b5e8d2ddaab2913d8a8bd8af
fuyishun <452625491@qq.com>
2016-12-28 17:50:10 +0800

    fys 注册、忘记密码界面

---
+++ app/src/main/java/com/jfq/msms/login/ForgetpasswordActivity.java
@@ -0,0 +1,15 @@
+package com.jfq.msms.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jfq.msms.R;
+
+public class ForgetpasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgetpassword);
+    }
+}
